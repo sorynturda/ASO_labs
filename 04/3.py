60b155d1f0a35998df9bf90cbcb155875cb73048
Sorin Turda <s.turda12@gmail.com>
2025-10-27 19:20:25 +0200

    lab 4 don3

--- 04/3.py
+++ 04/3.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+import sys
+import os
+from shutil import copytree
+
+def empty_dir(dir_path: str) -> bool:
+    return os.path.isdir(dir_path) and not os.listdir(dir_path)
+
+def main(dir_path: str):
+    dest = os.getcwd() + "/copy"
+    yes = input(f"copy {dir_path} to {dest}?:\n'yes' or 'no'\n") == "yes"
+    if not yes:
+        return
+    copytree(dir_path, dest)
+    for (root, dirs, filenames) in os.walk(dest):
+        for dir in dirs:
+            dirs_path = os.path.join(root, dir)
+            if empty_dir(dirs_path):
+                yes = input(f"do you want to remove {dirs_path} directory?: ") == "yes"
+                if yes:
+                    os.rmdir(dirs_path)
 
 if __name__ == "__main__":
-    print("OK")
+    if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} <dir_path>")
+    elif not os.path.isdir(sys.argv[1]):
+        print(f"{sys.argv[1]} is not a directory")
+    else:
+        main(sys.argv[1])
